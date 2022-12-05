5a528766c5a0b403bbc33e44a2040f5d176811c4
Artem Gorbatuk <gorbatuk.am@gmail.com>
2022-12-05 01:54:19 +0100

    Create Service Structure

---
+++ Services.Content/Api/ServiceHome.cs
@@ -0,0 +1,34 @@
+﻿using Repositories.Ef.Interfaces;
+using Services.Content.Domain;
+using Services.Content.Interfaces;
+
+namespace Services.Content.Api
+{
+    public class ServiceHome : IServiceHome
+    {
+        private readonly IRepositoryPerson repositoryPerson;
+
+        public ServiceHome(IRepositoryPerson repositoryPerson)
+        {
+            this.repositoryPerson = repositoryPerson;
+        }
+
+        public Task<HomeResult> GetAsync()
+        {
+            var personsTotal = repositoryPerson.CountAsync();
+
+            var aliveTotal = repositoryPerson.AliveTotalAsync();
+
+            var deadTotal = repositoryPerson.DeathTotalAsync();
+
+            Task.WaitAll(personsTotal, aliveTotal, deadTotal);
+
+            return Task.FromResult(new HomeResult()
+            {
+                PersonsTotal = personsTotal.Result,
+                AliveTotal = aliveTotal.Result,
+                DeadTotal = deadTotal.Result,
+            });
+        }
+    }
+}
---
+++ Services.Content/Domain/HomeResult.cs
@@ -0,0 +1,9 @@
+﻿namespace Services.Content.Domain
+{
+    public class HomeResult
+    {
+        public int PersonsTotal { get; set; }
+        public int AliveTotal { get; set; }
+        public int DeadTotal { get; set; }
+    }
+}
