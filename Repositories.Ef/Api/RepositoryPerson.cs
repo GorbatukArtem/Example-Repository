c7d53a831f106e93e2a04867cdd4936f6e378651
Artem Gorbatuk <gorbatuk.am@gmail.com>
2022-12-05 01:31:34 +0100

    Create Repository Structure

---
+++ Repositories.Ef/Api/RepositoryPerson.cs
@@ -0,0 +1,24 @@
+﻿using Datasource.Ef.Contexts;
+using Datasource.Ef.Domain.Content;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Ef.Repositories;
+using Repositories.Ef.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Ef.Api
+{
+    public class RepositoryPerson : RepositoryBase<Person, int>, IRepositoryPerson
+    {
+        public RepositoryPerson(DbContextPersonalCard context) : base(context) { }
+
+        public Task<int> AliveTotalAsync()
+        {
+            return Context.Persons
+                .CountAsync(p => p.Death == null && p.Birth != null && p.Birth.Value.Year > DateTime.Now.Year - 120);
+        }
+    }
+}
