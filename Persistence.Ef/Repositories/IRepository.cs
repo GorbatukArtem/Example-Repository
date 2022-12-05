ac2dc3abbf79e1f40e74c48f7d372c3b2b572eb0
Artem Gorbatuk <gorbatuk.am@gmail.com>
2022-12-05 01:23:42 +0100

    Create Persistence Structure

---
+++ Persistence.Ef/Repositories/IRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace Persistence.Ef.Repositories
+{
+    public interface IRepository<TEntity, in TKey> where TEntity : class
+    {
+        TEntity Add(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TKey id);
+
+        ValueTask<TEntity?> GetAsync(TKey id);
+        ValueTask<TEntity?> GetAsync(TKey id, CancellationToken token);
+
+        Task<List<TEntity>> GetAsync();
+        Task<List<TEntity>> GetAsync(CancellationToken token);
+
+        Task<int> CountAsync();
+        Task<int> CountAsync(CancellationToken token);
+    }
+}
